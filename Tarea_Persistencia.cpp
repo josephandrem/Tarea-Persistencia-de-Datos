01023cd5dfcc8c9702a27d626d72bfa92d9e115b
Joseph Segovia <segoviajoseph3@gmail.com>
2026-05-06 07:57:15 -0500

    Inicializacion del repositorio y creacion de variables

---
+++ Tarea_Persistencia.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+int main() {
+    string nombre, carrera;
+    int edad;
+    float promedio;
+
+    
+    return 0;
+}
